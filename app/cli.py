0d4b25ef6d808cd315953573052d9d3b0b358d1a
JoMingyu <>
2022-12-17 15:44:26 +0900

    퍼포먼스 개선

--- app/cli.py
+++ app/cli.py
@@ -1,4 +1,6 @@
+import time
 from itertools import islice
+from multiprocessing import Process
 from os import listdir
 from os.path import join
 
@@ -11,59 +13,67 @@ def chunk(it, size):
     return list(iter(lambda: tuple(islice(it, size)), ()))
 
 
-def square_and_resize_image(image: Image, resize_scale: int):
-    width, height = image.size
-
-    if width > height:
-        # 가로로 긴 이미지
-        pivot = (width - height) // 2
-        image = image.crop((pivot, 0, width - pivot, height))
-    elif width < height:
-        # 세로로 긴 이미지
-        pivot = (width - height) // 2
-        image = image.crop((0, pivot, width, height - pivot))
-
+def resize_image(image: Image, resize_scale: int):
     image = image.resize((resize_scale, resize_scale))
 
     return image
 
 
-def make_2by2_grid(images, grid_scale) -> Image:
-    margin_per_item = grid_scale // 80 // 4
+def make_2by2_grid(images, grid_scale, output_path) -> Image:
+    if len(images) == 1:
+        image = Image.open(images[0])
+        image.save(output_path)
+    else:
+        margin_per_item = grid_scale // 80 // 4
 
-    background = Image.new("RGBA", (grid_scale, grid_scale), (255, 255, 255, 255))
+        background = Image.new("RGBA", (grid_scale, grid_scale), (255, 255, 255, 255))
 
-    offsets = [
-        (0, 0),
-        (grid_scale // 2 + margin_per_item, 0),
-        (0, grid_scale // 2 + margin_per_item),
-        (grid_scale // 2 + margin_per_item, grid_scale // 2 + margin_per_item),
-    ]
+        offsets = [
+            (0, 0),
+            (grid_scale // 2 + margin_per_item, 0),
+            (0, grid_scale // 2 + margin_per_item),
+            (grid_scale // 2 + margin_per_item, grid_scale // 2 + margin_per_item),
+        ]
 
-    for index, _ in enumerate(images):
-        image = square_and_resize_image(
-            Image.open(_), grid_scale // 2 - margin_per_item
-        )
-        background.paste(image, offsets[index])
+        for index, _ in enumerate(images):
+            image = resize_image(Image.open(_), grid_scale // 2 - margin_per_item)
+            background.paste(image, offsets[index])
 
-    return background
+        background.save(output_path)
 
 
 def build_grids(source_path: str, output_path: str):
-    filenames = [join(source_path, f) for f in sorted(listdir(source_path))]
+    filenames = [
+        join(source_path, f) for f in sorted(listdir(source_path)) if f != ".DS_Store"
+    ]
 
-    chunks_of_grid_target_filenames = chunk(filenames, 4)
-
-    pil_images = []
-
-    for grid_target_filenames in chunks_of_grid_target_filenames:
-        if len(grid_target_filenames) < 4:
-            pil_images += [Image.open(filename) for filename in grid_target_filenames]
+    chunks = chunk(filenames, 4)
+    chunks_splited = []
+    for c in chunks:
+        if len(c) < 4:
+            chunks_splited += [(_,) for _ in c]
         else:
-            pil_images.append(make_2by2_grid(grid_target_filenames, 2048))
+            chunks_splited.append(c)
 
-    for seq, grid in enumerate(pil_images):
-        grid.save(join(output_path, f"grid-{seq}.png"))
+    processes = []
+
+    for index, grid_target_filenames in enumerate(chunks_splited):
+        processes.append(
+            Process(
+                target=make_2by2_grid,
+                args=(
+                    grid_target_filenames,
+                    2048,
+                    join(output_path, f"grid-{index}.png"),
+                ),
+            )
+        )
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
 
 
 @command()
